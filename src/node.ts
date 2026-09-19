d06a8dec76b8b5abd0735e5f9e096f311f33347d
William Roque <william.aroque@gmail.com>
2026-09-19 16:29:02 -0300

    Incorporate base and render nodes.

--- src/node.ts
+++ src/node.ts
@@ -5,7 +5,7 @@ export interface NodeDefinition {
     displayName: string;
     category: string;
     visible: boolean;
-    packageId: string;
+    packageId?: string;
     inputs: InputDefinition[];
     outputs: OutputDefinition[];
     properties: PropertyDefinition[];
@@ -13,4 +13,32 @@ export interface NodeDefinition {
     getInputs?(data?: Record<string, unknown>, connections?: any[]): InputDefinition[];
     getOutputs?(data?: Record<string, unknown>, connections?: any[]): OutputDefinition[];
     evaluate(inputs: Record<string, any>, properties: Record<string, any>): Promise<Record<string, any>>;
+}
+
+export abstract class BaseNode implements NodeDefinition {
+    abstract readonly typeId: string;
+    abstract readonly displayName: string;
+    abstract readonly category: string;
+    abstract readonly visible: boolean;
+    readonly packageId?: string;
+    abstract readonly inputs: InputDefinition[];
+    abstract readonly outputs: OutputDefinition[];
+    abstract readonly properties: PropertyDefinition[];
+
+    getInputs(data?: Record<string, unknown>, connections?: any[]): InputDefinition[] {
+        return this.inputs;
+    }
+
+    getOutputs(data?: Record<string, unknown>, connections?: any[]): OutputDefinition[] {
+        return this.outputs;
+    }
+
+    async evaluate(inputs: Record<string, any>, properties: Record<string, any>): Promise<Record<string, any>> {
+        return {};
+    }
+}
+
+export abstract class RenderNode extends BaseNode {
+    readonly category: string = 'Render';
+    readonly visible: boolean = false;
 }
