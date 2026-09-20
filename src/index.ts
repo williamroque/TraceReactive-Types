25c2ba07a0865982672b53b1a65c650135921e35
William Roque <william.aroque@gmail.com>
2026-09-20 16:50:47 -0300

    Fixed export issue.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export type { IOTypeDefinition, CoreIOType } from './io';
 export type { PropertyDefinition, InputDefinition, OutputDefinition } from './property';
 export type { NodeDefinition } from './node';
-export { BaseNode, RenderNode } from './node';
+export { BaseNode, RenderNode, ExecuteNode, EventNode } from './node';
 export type { ThemeVariableDefinition, ThemeSection } from './theme';
 export type { PreviewerDefinition, PreviewerProps } from './preview';
 export type { ExportOptions, ExporterDefinition } from './export';
